459803b0ebaceeff5a3819a643ce00941a524627
namilan <nichole.milan@gmail.com>
2020-08-31 19:06:33 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,24 @@ public class Main {
 		//write here. 
 		
 		int myVariable=50; 
+		if ( myVariable == 50) {
+			System.out.println(" Printed");
+		}
+		
 		myVariable++;
 		myVariable--;
 		System.out.println("This is a test");
 		
 		System.out.println("This is " + 
 				" another" +
-				"still more.");
+				" still more.");
+		
+		int anotherVariable = 5; myVariable--; System.out.println("This is another one");
+		
+		//white space above, the space between the statements. 
+		
+		//indentation, makes statements more readable. indent from method definitions. 
+		
 		
 	}
 
